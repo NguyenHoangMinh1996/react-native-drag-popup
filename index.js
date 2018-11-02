477d88c9bcc0d1ad1c10e8540e234aefbfb26a91
NguyenHoangMinhkkkk <NguyenHoangMinhkkkk@gmail.com>
2018-11-02 10:42:48 +0700

    fix

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import React from 'react';
 import ModalComponent from './Component';
 
-const ComponentRef = React.createRef()
-const openPopUp = ComponentRef.current.onOpenModal
-
-export { openPopUp }
 export default ModalComponent
